a72d14dcef834fd8e3f33179b6671c827f72117a
varung97 <varung97@yahoo.co.in>
2016-09-03 18:16:10 +0800

    Add sample API

--- app.py
+++ app.py
@@ -9,6 +9,14 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/sample', methods=['GET'])
+def sampleAPI():
+    # request.args.get('id', '')
+    return json.dumps([
+        "Hi I am a little basi who likes to whooski with his titchy doggy friends while playing football",
+        "Bye you little basi of a doggy catty bumyum"
+    ])
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
