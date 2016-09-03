5d14d045dc3465ad0194e12d97af5534498dfba0
Harish V <harishv207@gmail.com>
2016-09-03 15:21:54 +0800

    Splash

--- js/app.jsx
+++ js/app.jsx
@@ -1,18 +1,18 @@
 function getParameterByName(name, url) {
-    if (!url) url = window.location.href;
-    name = name.replace(/[\[\]]/g, "\\$&");
-    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-        results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, " "));
+	if (!url) url = window.location.href;
+	name = name.replace(/[\[\]]/g, "\\$&");
+	var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+	results = regex.exec(url);
+	if (!results) return null;
+	if (!results[2]) return '';
+	return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 
 var App = React.createClass({
 	getInitialState() {
-	    return {
+		return {
 
-	    };
+		};
 	},
 	componentWillMount() {
 		// var apiUrl = "https://api.github.com/users/" + gitHubUserName + "/repos";
@@ -40,11 +40,52 @@ var App = React.createClass({
 
 		return (
 			<div>
-				HI
+			<Home />
 			</div>
 		);
 	}
 });
+
+var Home = React.createClass({
+	render: function() {
+		return (
+			<div>
+			<div className="cover">
+			<div className="cover-image"></div>
+			<div className="container">
+			<div className="row">
+			<div className="col-md-12 text-center">
+			<h1 className="text-inverse heading">Tweet Battle</h1>
+			<p className="text-inverse">The Epic Tweet Battle</p>
+			</div>
+			</div>
+			<div className="row text-center">
+			<div className="col-md-4">
+			<div className="form-group">
+			<input type="text" className="custom-input" id="usr"></input>
+			</div>
+			</div>
+			<div className="col-md-4 text-inverse">
+			<h1>VS</h1>
+			</div>
+			<div className="col-md-4">
+			<div className="form-group">
+			<input type="text" className="custom-input" id="usr"></input>
+			</div>
+			</div>
+			</div>
+			<div className="row">
+			<div className="col-md-12 text-center">
+			<a className="custom-btn description" href="#app">Go!</a>
+			</div>              
+			</div>
+			</div>
+			</div>
+			</div>
+			);
+	}
+	});
+
 
 
 ReactDOM.render(<App />, document.getElementById("app"));
