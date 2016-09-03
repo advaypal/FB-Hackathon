a0a4d92a1dd2f04fce742dcb656c0afefd55f84c
varung97 <varung97@yahoo.co.in>
2016-09-03 18:29:31 +0800

    Update sample API

--- app.py
+++ app.py
@@ -11,11 +11,18 @@ def index():
 
 @app.route('/sample', methods=['GET'])
 def sampleAPI():
-    # request.args.get('id', '')
-    return json.dumps([
-        "Hi I am a little basi who likes to whooski with his titchy doggy friends while playing football",
-        "Bye you little basi of a doggy catty bumyum"
-    ])
+    id1 = request.args.get('id1', '')
+    id2 = request.args.get('id2', '')
+    return json.dumps({
+        id1: {
+            'tweet': "Hi I am a little basi who likes to whooski with his titchy doggy friends while playing football",
+            'img': 'http://i.cdn.turner.com/v5cache/CARTOON/site/Images/i80/tj_tom_180x180.png'
+        },
+        id2: {
+            'tweet': "Bye you little basi of a doggy catty bumyum",
+            'img': 'http://www.easydrawingtutorials.com/images/Jerry/jerry-thumb.jpg'
+        }
+    })
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
